82e8ad917ef8cc7ae34d9cf10bf140a03bfb9687
karpukhinda <karpuhinda@yandex.ru>
2022-10-27 18:20:32 +0300

    Домашка

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
+////Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int x1 = a / 10;
+// int x2 = x1 % 10;
+// Console.WriteLine(x2);
+
+
+
+////Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.Write("Ввдите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (99 < number)
+// {
+// string str = Convert.ToString(number);
+// Console.WriteLine($"третья цифра этого числа {str[2]}");
+// }
+// else Console.Write("третьей цифры нет");
+
+
+
+////Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.WriteLine("Ввдите число от 1 до 7 :");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number <= 5 )
+// Console.Write("Будний день");
+// if (number > 5 && 8> number)
+// Console.Write("Выходной");
+// if (number >7)
+// Console.Write("Такого дня недели нет. Введите число от 1 до 7");
+
+
+
+
+
